729fc3e5c1d66032911f48866dad38509f395d1e
ravellys <lucas.ravellys@ufpe.br>
2020-08-21 22:52:43 -0300

    criados atributos de Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,8 +1,17 @@
 class Pessoa:
-    def cumprimentar( self ):
+    def __init__(self, nome=None, idade=25):
+       self.idade = idade
+       self.nome = nome
+
+    def cumprimentar(self):
         return f'olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
-
+    p = Pessoa('Luciano')
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'renzo'
+    print(p.nome)
+    print(p.idade)
